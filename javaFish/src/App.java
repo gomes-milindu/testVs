4409de012cab2c71d5105c3367941ba70836938a
Milindu <gomesmilindu@gmail.com>
2025-05-27 11:01:08 +0530

    starPattern

--- javaFish/src/App.java
+++ javaFish/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        for(int x=0; x<5; x++){
+            for(int y=x; y<5; y++){
+                    System.out.println("*");
+            }
+            System.out.println("\n");
+        }
     }
 }
